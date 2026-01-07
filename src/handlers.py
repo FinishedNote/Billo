3e5ff32c783dfb2f7de88917038446c871abed8c
Miroir Raphaël <miroir.raphael07@gmail.com>
2026-01-07 20:02:25 +0100

    feat: Update invoice templates: modify test.html title and add test2.html and test3.html

--- src/handlers.py
+++ src/handlers.py
@@ -1,21 +1,28 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes, ConversationHandler
 from telegram.constants import ParseMode
-from telegram.ext import ContextTypes
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
+from io import BytesIO
+from datetime import datetime
 
-async def generate(update: Update, context: ContextTypes.DEFAULT_TYPE):
+TEMPLATE, CUSTOMER_NAME, SERVICE, PRICE = range(4)
+
+message = (
+    "<b>Salut Raph !</b> 👋\n"
+    "Quel type de facture veux tu générer aujourd'hui ? \n"
+    "Choisi une des options ci-dessous :"
+)
+
+async def generate_receipt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
     keyboard = [
-        [InlineKeyboardButton("Moncler", callback_data='mc')],
-        [InlineKeyboardButton("Golden Goose", callback_data='gg')],
-        [InlineKeyboardButton("Stock x", callback_data='sx')]
+        [InlineKeyboardButton("✨ Moncler", callback_data='moncler.html')],
+        [InlineKeyboardButton("✨ Golden Goose", callback_data='test2.html')],
+        [InlineKeyboardButton("✨ Stock X", callback_data='test3.html')]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-
-    message = (
-        "<b>Salut Raph !</b> 👋\n"
-        "Quel type de facture veux tu générer aujourd'hui ? \n"
-        "Choisi une des options ci-dessous :"
-    )
 
     await update.message.reply_text(
         text=message,
@@ -23,14 +30,66 @@ async def generate(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_markup=reply_markup
     )
 
-async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    return TEMPLATE
+
+
+async def get_product(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-    
     await query.answer()
+
+    active_template = query.data
+    context.user_data['template'] = active_template
+
+    await query.edit_message_text(f"Quel est le nom de l'acheteur (nom + prénom) ?")
     
-    if query.data == 'mc':
-        await query.edit_message_text(text="Facture Moncler en cours de création...")
-    elif query.data == 'gg':
-        await query.edit_message_text(text="Facture Golden Goose en cours de création...")
-    elif query.data == 'sx':
-        await query.edit_message_text(text="Facture Stock x en cours de création...")
+    return CUSTOMER_NAME
+
+async def get_client_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    name = update.message.text
+    context.user_data['customer_name'] = name
+    
+    await update.message.reply_text(f"Quel est le nom de l'article ?")
+    return SERVICE
+
+async def get_price(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data['service'] = update.message.text
+    await update.message.reply_text("Quel est le Prix ?")
+    return PRICE
+
+async def generate_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    customer_name = context.user_data.get('customer_name', 'Client Inconnu')
+    price = update.message.text
+    service = context.user_data['service']
+
+    template_name = context.user_data.get('template', 'simple.html')
+
+    date_jour = datetime.now().strftime("%d/%m/%Y")
+
+    await update.message.reply_text("Génération en cours...")
+
+    try:
+        env = Environment(loader=FileSystemLoader('src/templates'))
+        template = env.get_template(template_name)
+        
+        html_out = template.render(
+            customer_name=customer_name, service=service, price=price, date=date_jour
+        )
+        
+        file_buffer = BytesIO(html_out.encode('utf-8'))
+        file_buffer.name = f"Facture_{service}.html"
+        file_buffer.seek(0)
+        
+        await update.message.reply_document(
+            document=file_buffer,
+            filename=f"Facture_{service}.html",
+            caption="Voici ta facture ! Ouvre-la avec ton navigateur 🌐"
+        )
+
+    except Exception as e:
+        await update.message.reply_text(f"Erreur technique : {e}")
+
+    return ConversationHandler.END
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Annulé.")
+    return ConversationHandler.END
--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler
+from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, CallbackQueryHandler, filters
 from src.config import TOKEN
-from src.handlers import generate, button_handler
+from src.handlers import generate_receipt, get_product, get_client_name, get_price, generate_pdf, cancel, TEMPLATE, CUSTOMER_NAME, SERVICE, PRICE
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("generate", generate))
-    app.add_handler(CallbackQueryHandler(button_handler))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('generate_receipt', generate_receipt)],
+        states={
+            TEMPLATE: [CallbackQueryHandler(get_product)],
+            CUSTOMER_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_client_name)],
+            SERVICE: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_price)],
+            PRICE: [MessageHandler(filters.TEXT & ~filters.COMMAND, generate_pdf)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
 
-    print("Bot Démarré...")
+    app.add_handler(conv_handler)
     app.run_polling()
 
 if __name__ == "__main__":
